4bdca1502922ece552fa29dd58306946e73a2179
Jarlyyn <jarlyyn@qq.com>
2019-07-01 19:32:52 +0800

    update

---
+++ modules/project/skeleton.go
@@ -0,0 +1,19 @@
+package project
+
+var AppSkeleton = map[string]string{
+	"/skeleton/.gitignore":                               "/.gitignore",
+	"/skeleton/appdata/readme.md":                        "/appdata/readme.md",
+	"/skeleton/config/development.toml":                  "/config/development.toml",
+	"/skeleton/resources/data/readme.md":                 "/resources/data/readme.md",
+	"/skeleton/src/test/config/readme.md":                "/src/test/config/readme.md",
+	"/skeleton/src/main.go":                              "/src/main.go",
+	"/skeleton/src/drivers.go":                           "/src/drivers.go",
+	"/skeleton/src/errors.go":                            "/src/errors.go",
+	"/skeleton/src/vendor/app/development.go":            "/src/vendor/app/development.go",
+	"/skeleton/src/vendor/app/loadconfigs.go":            "/src/vendor/app/loadconfigs.go",
+	"/skeleton/src/vendor/app/time.go":                   "/src/vendor/app/time.go",
+	"/skeleton/src/vendor/appevents/appevents.go":        "/src/vendor/appevents/appevents.go",
+	"/skeleton/src/vendor/appevents/registeredevents.go": "/src/vendor/appevents/registeredevents.go",
+	"/skeleton/system/constants/time.toml":               "/system/constants/time.toml",
+	"/skeleton/system/config.examples/development.toml":  "/system/config.examples/development.toml",
+}
