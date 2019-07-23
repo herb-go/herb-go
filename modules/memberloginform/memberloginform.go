34eacf04b2fb44401103fd8d9a2bfbb68b6bb633
Jarlyyn <jarlyyn@qq.com>
2019-07-24 01:22:03 +0800

    update

--- modules/memberloginform/memberloginform.go
+++ modules/memberloginform/memberloginform.go
@@ -1,1 +1,127 @@
 package memberloginform
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/herb-go/util/cli/name"
+
+	"github.com/herb-go/herb-go/modules/project"
+	"github.com/herb-go/util/cli/app"
+	"github.com/herb-go/util/cli/app/tools"
+)
+
+type LoginForm struct {
+	app.BasicModule
+	FormID        string
+	ByUID         bool
+	Casesensitive bool
+	SlienceMode   bool
+}
+
+func (m *LoginForm) ID() string {
+	return "github.com/herb-go/herb-go/modules/memberloginform"
+}
+
+func (m *LoginForm) Cmd() string {
+	return "memberloginform"
+}
+
+func (m *LoginForm) Help(a *app.Application) string {
+	m.Init(a, &[]string{})
+	help := `Usage %s memberloginform [name].
+Create new member login form and action.
+File below will be created:
+	src/vendor/modules;[name]/[id]/forms/login.form
+	src/vendor/modules/[name]/[id]/actions/login.action
+`
+	return fmt.Sprintf(help, a.Config.Cmd)
+}
+
+func (m *LoginForm) Desc(a *app.Application) string {
+	return "Create new member login form and action"
+}
+func (m *LoginForm) Group(a *app.Application) string {
+	return "Auth"
+}
+func (m *LoginForm) Init(a *app.Application, args *[]string) error {
+	if m.FlagSet().Parsed() {
+		return nil
+	}
+	m.FlagSet().BoolVar(&m.SlienceMode, "s", false, "Slience mode")
+
+	err := m.FlagSet().Parse(*args)
+	if err != nil {
+		return err
+	}
+	*args = m.FlagSet().Args()
+	return nil
+}
+func (m *LoginForm) Question(a *app.Application) error {
+	return nil
+}
+func (m *LoginForm) Exec(a *app.Application, args []string) error {
+	err := m.Init(a, &args)
+	if err != nil {
+		return err
+	}
+	if len(args) == 0 {
+		a.PrintModuleHelp(m)
+		return nil
+	}
+	n, err := name.New(true, args...)
+	if err != nil {
+		return err
+	}
+	mp, err := project.GetModuleFolder(a.Cwd)
+	if err != nil {
+		return err
+	}
+	app, err := tools.FindLib(a.Getenv("GOPATH"), "github.com/herb-go/herb-go")
+	if err != nil {
+		return err
+	}
+
+	task := tools.NewTask(filepath.Join(app, "/modules/memberloginform/resources"), a.Cwd)
+
+	err = m.Render(a, a.Cwd, mp, task, n)
+	if err != nil {
+		return err
+	}
+	task.AddJob(func() error {
+		a.Printf("Member login form  \"%s\" created.\n", n.LowerWithParentDotSeparated)
+		return nil
+	})
+	err = task.ErrosIfAnyFileExists()
+	if err != nil {
+		return err
+	}
+	ok, err := task.ConfirmIf(a, !m.SlienceMode)
+	if err != nil {
+		return err
+	}
+	if !ok {
+		return nil
+	}
+	return task.Exec()
+
+}
+
+func (m *LoginForm) Render(a *app.Application, appPath string, mp string, task *tools.Task, n *name.Name) error {
+	var configgopath string
+
+	filesToRender := map[string]string{
+		filepath.Join("system", "constants", n.LowerWithParentDotSeparated+".toml"): "config.toml.tmpl",
+		filepath.Join(mp, "app", n.LowerWithParentDotSeparated+".go"):               configgopath,
+	}
+	data := map[string]interface{}{
+		"Name": n,
+	}
+	return task.RenderFiles(filesToRender, data)
+}
+
+var LoginFormModule = &LoginForm{}
+
+func init() {
+	app.Register(LoginFormModule)
+}
