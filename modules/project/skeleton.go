0fc277c89de8b6760f381b9207fbb9f2a683955d
Jarlyyn <jarlyyn@qq.com>
2019-07-27 17:29:55 +0800

    update

--- modules/project/skeleton.go
+++ modules/project/skeleton.go
@@ -23,6 +23,10 @@ var AppSkeleton = func(mp string) map[string]string {
 		mp + "/test/test.go":                       "/skeleton/src/vendor/modules/test/test.go",
 		mp + "/test/drivers.go":                    "/skeleton/src/vendor/modules/test/drivers.go",
 		mp + "/test/init_test.go":                  "/skeleton/src/vendor/modules/test/init_test.go",
+		mp + "/test/readme.md":                     "/skeleton/src/vendor/modules/test/readme.md",
+		mp + "/test/tests/init.go":                 "/skeleton/src/vendor/modules/test/tests/test.go",
+		mp + "/test/tests/init_test.go":            "/skeleton/src/vendor/modules/test/tests/init_test.go",
+		mp + "/test/tests/readme.md":               "/skeleton/src/vendor/modules/test/tests/readme.md",
 	}
 }
 
