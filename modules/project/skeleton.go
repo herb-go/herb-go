e06a78e51ac93b959ac1a4ccc357f2a89512c654
Jarlyyn <jarlyyn@qq.com>
2019-07-28 01:12:38 +0800

    update

--- modules/project/skeleton.go
+++ modules/project/skeleton.go
@@ -2,7 +2,7 @@ package project
 
 var AppSkeleton = func(mp string) map[string]string {
 	return map[string]string{
-		"/.gitignore.example":                      "/skeleton/.gitignore",
+		"/.gitignore":                              "/skeleton/.gitignore.example",
 		"/appdata/readme.md":                       "/skeleton/appdata/readme.md",
 		"/config/readme.md":                        "/skeleton/config/readme.md",
 		"/config/development.toml":                 "/skeleton/config/development.toml",
