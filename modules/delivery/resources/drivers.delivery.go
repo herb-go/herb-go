2acf527b075402d6094b8319086a1a1375db30b3
jarlyyn <jarlyyn@qq.com>
2021-01-16 01:08:42 +0800

    update

--- modules/delivery/resources/drivers.delivery.go
+++ modules/delivery/resources/drivers.delivery.go
@@ -1,6 +1,7 @@
 package drivers
 
 import _ "github.com/herb-go/notification-drivers/delivery/loggerdelivery"     //logger delivery
+import _ "github.com/herb-go/notification-drivers/delivery/mockdelivery"       //mock delivery
 import _ "github.com/herb-go/notification-drivers/delivery/emaildelivery"      //eamil delivery
 import _ "github.com/herb-go/notification-drivers/delivery/wechattmdelivery"   //wechattm delivery
 import _ "github.com/herb-go/notification-drivers/delivery/wechatworkdelivery" //wechatwork delivery
