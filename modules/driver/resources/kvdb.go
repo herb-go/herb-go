8c7da337e392f36805450f34175b0e6f4c00f901
jarlyyn <jarlyyn@qq.com>
2021-02-07 22:42:19 +0800

    update

--- modules/driver/resources/kvdb.go
+++ modules/driver/resources/kvdb.go
@@ -4,3 +4,4 @@ import _ "github.com/herb-go/herbdata/kvdb/commonkvdb" //immemory kvdb
 // import _ "github.com/herb-go/herbdata-drivers/kvdb-drivers/freecachedb" //freecache kvdb
 // import _ "github.com/herb-go/herbdata-drivers/kvdb-drivers/leveldb"     //leveldb kvdb
 // import _ "github.com/herb-go/herbdata-drivers/kvdb-drivers/redisdb"     //redisdb kvdb
+// import _ "github.com/herb-go/herbdata-drivers/kvdb-drivers/badgerdb"     //badgerdb kvdb
